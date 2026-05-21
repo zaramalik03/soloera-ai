e210d5986fde6cc8735f5a47bcf17661dcfc2835
zaramalik03 <zmalik1@vols.utk.edu>
2026-05-21 11:46:39 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'soloera-pink': '#F4C2C2',
+        'soloera-cream': '#FFF5E1',
+        'soloera-sage': '#B5C9A2',
+      }
+    },
+  },
+  plugins: [],
+}
